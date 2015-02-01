0338bdc9df80444b7bf746e23229f7ce69b3b824
Murzic <e.ungureanu92@gmail.com>
2015-02-01 14:50:31 +0100

    Generated the Question, Answer and Group models

---
+++ db/migrate/20150201134622_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.text :name
+      t.binary :image
+      t.references :quiz, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :questions, :quizzes
+  end
+end
