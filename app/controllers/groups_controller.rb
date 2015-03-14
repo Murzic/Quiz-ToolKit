085ad721c1866ad1333b5706f8cbc5dd19ada725
Murzic <e.ungureanu92@gmail.com>
2015-03-14 23:18:49 +0200

    The question groups can now be removed from the question, but it remains in the quiz for later use

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,27 +1,28 @@
 class GroupsController < ApplicationController
-	def create
-		@question = Question.find(params[:question_id])
-		@quiz = @question.quiz
-		@quiz_groups = @quiz.groups
-		if @quiz_groups.exists?(group_params)
-			@question.groups << @quiz_groups.where(group_params)
-		else
-			@quiz_groups.create(group_params)
-			@question.groups << @quiz_groups.where(group_params)
-		end
-		redirect_to edit_question_path(@question)	
-	end
+  def create
+    @question = Question.find(params[:question_id])
+    @quiz = @question.quiz
+    @quiz_groups = @quiz.groups
+    if @quiz_groups.exists?(group_params)
+      @question.groups << @quiz_groups.where(group_params)
+    else
+      @quiz_groups.create(group_params)
+      @question.groups << @quiz_groups.where(group_params)
+    end
+    redirect_to edit_question_path(@question) 
+  end
 
-	def update
-		@group = Group.find(params[:id])
-		@question = params[:question_id]
-		@group.update(group_params)
-		redirect_to edit_question_path(@question)
-	end
+  def destroy
+    @question = Question.find(params[:question_id])
+    @group = Group.find(params[:id])
+    @question.groups.destroy(@group)
+    redirect_to edit_question_path(@question) 
+  end
 
-	private
 
-	def group_params
-		params.require(:group).permit(:name)
-	end
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
