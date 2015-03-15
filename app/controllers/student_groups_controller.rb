585bb5d8cfab25e0f0cf9f41ee878382c1de7c2d
Murzic <e.ungureanu92@gmail.com>
2015-03-15 15:00:04 +0200

    Student groups can be editet and removed

--- app/controllers/student_groups_controller.rb
+++ app/controllers/student_groups_controller.rb
@@ -9,6 +9,29 @@ class StudentGroupsController < ApplicationController
   	redirect_to student_groups_path
   end
 
+  def show
+  end
+
+  def update
+  	@student_group = StudentGroup.find(params[:id])
+  	if @student_group.update(student_group_params)
+  		redirect_to student_groups_path
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def edit
+  	@student_group = StudentGroup.find(params[:id])
+  	@student_groups = StudentGroup.all
+  end
+
+  def destroy
+  	@student_group = StudentGroup.find(params[:id])
+  	@student_group.destroy
+  	redirect_to student_groups_path
+  end
+
   private
 
   def student_group_params
