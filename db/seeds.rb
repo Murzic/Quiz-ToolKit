9b4b7300905b945ed8f926fc4a5ed3e391207e2c
Murzic <e.ungureanu92@gmail.com>
2015-03-28 13:20:09 +0200

    Changed the seeds.rb file. Now adding default questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 user = User.create email: "user@example.com", password: "userexample",password_confirmation: "userexample"
+course = user.courses.create(name: "QC")
+quiz = course.quizzes.create(name: "Quiz nr. 1")
+range = 1..30
+array = Array.new
+range.each do |n|
+  array << {name: "Question nr #{n}"}
+end
+quiz.questions.create(array)
+student_group = course.student_groups.create(name: "FAF111")
 
