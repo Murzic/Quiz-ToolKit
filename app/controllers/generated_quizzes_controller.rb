aa1ebc128e81a0f2a6926d73e916645cbe44dcbb
Murzic <e.ungureanu92@gmail.com>
2015-03-28 11:54:55 +0200

    When specifying 'Number of copies', the copies table will be populated

--- app/controllers/generated_quizzes_controller.rb
+++ app/controllers/generated_quizzes_controller.rb
@@ -3,6 +3,7 @@ class GeneratedQuizzesController < ApplicationController
     @user = current_user
     @generated_quiz = @user.generated_quizzes.new(generated_quiz_params)
     @generated_quiz.save
+    GeneratedQuizzesHelper.gen_copies(@generated_quiz)
     redirect_to :back
   end
 
--- app/helpers/generated_quizzes_helper.rb
+++ app/helpers/generated_quizzes_helper.rb
@@ -1,2 +1,9 @@
 module GeneratedQuizzesHelper
+  def self.gen_copies(genquiz)
+    @attributes = genquiz.attributes.except("created_at", "updated_at")
+    questions = Quiz.find(@attributes["quiz_id"]).question_ids
+    if @attributes["copies_nr"]
+      genquiz.copies.create(questions: questions, samples_nr: @attributes["copies_nr"])
+    end
+  end
 end
