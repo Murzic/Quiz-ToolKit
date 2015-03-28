c471a3be6deccf44bac7c9397b99fbece81ca14d
Murzic <e.ungureanu92@gmail.com>
2015-03-28 18:39:22 +0200

    The copies are created based on the selected student groups

--- app/helpers/generated_quizzes_helper.rb
+++ app/helpers/generated_quizzes_helper.rb
@@ -12,5 +12,16 @@ module GeneratedQuizzesHelper
         genquiz.copies.create(questions: questions, samples_nr: @attributes["copies_nr"])
       end
     end
+  
+    if @attributes["student_group_ids"] && !@attributes["student_group_ids"].first.empty?
+      array = Array.new
+      @attributes["student_group_ids"].each do |sgid|
+        StudentGroup.find(sgid).student_ids.each do |sid|
+          array << {questions: questions, samples_nr: 1, student_id: sid, student_group_id: sgid}
+        end
+      end
+      genquiz.copies.create(array)
+    end
+
   end
 end
