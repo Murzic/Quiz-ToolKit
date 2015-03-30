f61d2986b1cd2d3dc416fcb29587ae47fde99ccf
Murzic <e.ungureanu92@gmail.com>
2015-03-30 17:12:43 +0300

    Student groups option now works side by side with random option and questions_nr option

--- app/helpers/generated_quizzes_helper.rb
+++ app/helpers/generated_quizzes_helper.rb
@@ -2,7 +2,9 @@ module GeneratedQuizzesHelper
   def self.gen_copies(genquiz)
     @attributes = genquiz.attributes.except("created_at", "updated_at")
     questions = Quiz.find(@attributes["quiz_id"]).question_ids
+    groups = Quiz.find(@attributes["quiz_id"]).group_ids
 
+    ### The case when the "copies nr" option is given with or without the random option
     if @attributes["copies_nr"]
       if @attributes["random_opt"]
         @attributes["copies_nr"].times do
@@ -13,12 +15,28 @@ module GeneratedQuizzesHelper
       end
     end
   
-
+    ### One or more "Student groups" are selected and various options
     if @attributes["student_group_ids"] && !@attributes["student_group_ids"].first.empty?
-      ActiveRecord::Base.transaction do
-        @attributes["student_group_ids"].each do |sgid|
-          StudentGroup.find(sgid).student_ids.each do |sid|
-            genquiz.copies.create(questions: questions, samples_nr: 1, student_id: sid, student_group_id: sgid)
+      ques_nr = 0
+      if @attributes["questions_nr"]
+        ques_nr = @attributes["questions_nr"]
+      else
+        ques_nr = questions.length
+      end
+      if @attributes["random_opt"]
+        ActiveRecord::Base.transaction do
+          @attributes["student_group_ids"].each do |sgid|
+            StudentGroup.find(sgid).student_ids.each do |sid|
+              genquiz.copies.create(questions: questions.shuffle.first(ques_nr), samples_nr: 1, student_id: sid, student_group_id: sgid)
+            end
+          end
+        end 
+      else
+        ActiveRecord::Base.transaction do
+          @attributes["student_group_ids"].each do |sgid|
+            StudentGroup.find(sgid).student_ids.each do |sid|
+              genquiz.copies.create(questions: questions.first(ques_nr), samples_nr: 1, student_id: sid, student_group_id: sgid)
+            end
           end
         end
       end
