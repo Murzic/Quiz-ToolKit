2f99bb9bc70aee2f82fc18cfb8dbb1d8c3f4f4c1
Murzic <e.ungureanu92@gmail.com>
2015-03-31 11:10:21 +0300

    Questions nr, versions_nr are implemented in regards with copies_nr and random options

--- app/helpers/generated_quizzes_helper.rb
+++ app/helpers/generated_quizzes_helper.rb
@@ -4,18 +4,37 @@ module GeneratedQuizzesHelper
     questions = Quiz.find(@attributes["quiz_id"]).question_ids
     groups = Quiz.find(@attributes["quiz_id"]).group_ids
 
-    ### The case when the "copies nr" option is given with or without the random option
+    ### The case when the "copies nr" option is given with or without options: (random_opt, questions_nr, versions_nr, question_groups_nrs)
     if @attributes["copies_nr"]
+      ques_nr = 0
+      questions_arr = Array.new
+      if @attributes["questions_nr"]
+        ques_nr = @attributes["questions_nr"]
+      else
+        ques_nr = questions.length
+      end
+      #### versions_nr given
+      if @attributes["versions_nr"]
+        @attributes["versions_nr"].times do
+          questions_arr << questions.shuffle.first(ques_nr)
+        end        
+        (0..@attributes["copies_nr"]).each do |i|
+          genquiz.copies.create(questions: questions_arr[i%4], samples_nr: 1)
+        end
+        return
+      end
+
+      #### Random_option given
       if @attributes["random_opt"]
         @attributes["copies_nr"].times do
-          genquiz.copies.create(questions: questions.shuffle, samples_nr: 1)
+          genquiz.copies.create(questions: questions.shuffle.first(ques_nr), samples_nr: 1)
         end
       else
-        genquiz.copies.create(questions: questions, samples_nr: @attributes["copies_nr"])
+        genquiz.copies.create(questions: questions.first(ques_nr), samples_nr: @attributes["copies_nr"])
       end
     end
   
-    ### One or more "Student groups" are selected and various options
+    ### One or more "Student groups" are selected and various options: (random_opt, questions_nr, versions_nr, question_groups_nrs)
     if @attributes["student_group_ids"] && !@attributes["student_group_ids"].first.empty?
       ques_nr = 0
       if @attributes["questions_nr"]
