fb0593694e74539af59ac15a62c0393075a05eb7
Murzic <e.ungureanu92@gmail.com>
2015-04-01 17:39:03 +0300

    Edited seeds.rb. Now it inserts more data in the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,52 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Base.transaction do
+  user = User.create email: "user@example.com", password: "userexample",password_confirmation: "userexample"
+  course = user.courses.create(name: "QC")
+  quiz = course.quizzes.create(name: "Quiz nr. 1")
+  range = 1..30
+  range2 = 1..4
+  array = Array.new
+  range.each do |n|
+    array << {name: "Question nr #{n}"}
+  end
 
-user = User.create email: "user@example.com", password: "userexample",password_confirmation: "userexample"
-course = user.courses.create(name: "QC")
-quiz = course.quizzes.create(name: "Quiz nr. 1")
-range = 1..30
-array = Array.new
-range.each do |n|
-  array << {name: "Question nr #{n}"}
+  quiz.groups.create([{name: "default"}, {name: "easy"}, {name: "medium"}, {name: "hard"}])
+
+  quiz.questions.create(array) # Creates the questions
+  quiz.questions.each do |question| # Creates the answers for each question
+    array2 = Array.new
+    range2.each do |n|
+      if rand(4) == 0
+        array2 << {name: "Answer nr #{n}", correct: true}
+      else
+        array2 << {name: "Answer nr #{n}"}
+      end
+    end  
+    question.answers.create(array2)
+    question.groups << quiz.groups.where(name: "default")
+    case question.id
+    when 1..10
+      question.groups << quiz.groups.where(name: "easy")
+    when 11..20
+      question.groups << quiz.groups.where(name: "medium")
+    when 21..30
+      question.groups << quiz.groups.where(name: "hard")  
+    end
+  end
+
+  student_group = course.student_groups.create(name: "FAF111")
+  student_group.students.create([
+                                {name: "Eugeniu", surname: "Ungureanu"},
+                                {name: "Sergiu", surname: "Terman"},
+                                {name: "Mihai", surname: "Iachimovschi"},
+                                {name: "Vladimir", surname: "Tribusean"},
+                                {name: "Vasile", surname: "Mardari"},
+                                {name: "Ion", surname: "Marusic"},
+                                {surname: "Negrei", name: "Petru"},
+                                {surname: "Plesco", name: "Alexandra"},
+                                {surname: "Truhin", name: "Alexei"},
+                                {surname: "Rusu", name: "Dorin"},
+                                {surname: "Safeaniuc", name: "Alexandru"}])
 end
-quiz.questions.create(array)
-student_group = course.student_groups.create(name: "FAF111")
-
