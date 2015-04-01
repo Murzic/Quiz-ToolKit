a49af3f32232538d44badf2434be3972934403c8
Murzic <e.ungureanu92@gmail.com>
2015-04-01 13:30:04 +0300

    The gen_copies method has been trimmed a bit

--- app/helpers/generated_quizzes_helper.rb
+++ app/helpers/generated_quizzes_helper.rb
@@ -1,37 +1,35 @@
 module GeneratedQuizzesHelper
   def self.gen_copies(genquiz)
     @attributes = genquiz.attributes.except("created_at", "updated_at")
-    questions = Quiz.find(@attributes["quiz_id"]).question_ids
     ques_nr = 0
     questions_arr = Array.new
     questions_gr = Array.new
 
+    ### Question groups numbers given
+    if !@attributes["question_groups_nrs"].empty?
+      @attributes["question_groups_nrs"].each do |key, value|
+        questions_gr.concat(Group.find(key.to_i).question_ids.shuffle.first(value.to_i))
+      end
+    else
+      questions_gr = Quiz.find(@attributes["quiz_id"]).question_ids
+    end
+
+    ### Questions number given
+    if @attributes["questions_nr"]
+      ques_nr = @attributes["questions_nr"]
+    else
+      ques_nr = questions_gr.length
+    end
+
     ### The case when the "copies nr" option is given with or without options: (random_opt, questions_nr, versions_nr, question_groups_nrs)
     if @attributes["copies_nr"]
-
-      #### Questions number given
-      if @attributes["questions_nr"]
-        ques_nr = @attributes["questions_nr"]
-      else
-        ques_nr = questions.length
-      end
-
-      #### Question groups numbers given
-      if !@attributes["question_groups_nrs"].empty?
-        @attributes["question_groups_nrs"].each do |key, value|
-          questions_gr.concat(Group.find(key.to_i).question_ids.shuffle.first(value.to_i))
-        end
-      else
-        questions_gr = questions
-      end
-
       #### versions_nr given
       if @attributes["versions_nr"]
         @attributes["versions_nr"].times do
           questions_arr << questions_gr.shuffle.first(ques_nr)
         end        
         ActiveRecord::Base.transaction do
-          (0..@attributes["copies_nr"]).each do |i|
+          (0...@attributes["copies_nr"]).each do |i|
             genquiz.copies.create(questions: questions_arr[i % @attributes["versions_nr"]], samples_nr: 1)
           end
         end
@@ -52,24 +50,9 @@ module GeneratedQuizzesHelper
           end
         end
       end
+
     ### One or more "Student groups" are selected and various options: (random_opt, questions_nr, versions_nr, question_groups_nrs)
     elsif @attributes["student_group_ids"] && !@attributes["student_group_ids"].first.empty?
-
-      if @attributes["questions_nr"]
-        ques_nr = @attributes["questions_nr"]
-      else
-        ques_nr = questions.length
-      end
-
-      #### Question groups numbers given
-      if !@attributes["question_groups_nrs"].empty?
-        @attributes["question_groups_nrs"].each do |key, value|
-          questions_gr.concat(Group.find(key.to_i).question_ids.shuffle.first(value.to_i))
-        end
-      else
-        questions_gr = questions
-      end
-
       #### versions_nr given
       if @attributes["versions_nr"]
         @attributes["versions_nr"].times do
@@ -89,7 +72,7 @@ module GeneratedQuizzesHelper
         ActiveRecord::Base.transaction do
           @attributes["student_group_ids"].each do |sgid|
             StudentGroup.find(sgid).student_ids.each do |sid|
-              genquiz.copies.create(questions: questions.shuffle.first(ques_nr), samples_nr: 1, student_id: sid, student_group_id: sgid)
+              genquiz.copies.create(questions: questions_gr.shuffle.first(ques_nr), samples_nr: 1, student_id: sid, student_group_id: sgid)
             end
           end
         end 
@@ -97,12 +80,11 @@ module GeneratedQuizzesHelper
         ActiveRecord::Base.transaction do
           @attributes["student_group_ids"].each do |sgid|
             StudentGroup.find(sgid).student_ids.each do |sid|
-              genquiz.copies.create(questions: questions.first(ques_nr), samples_nr: 1, student_id: sid, student_group_id: sgid)
+              genquiz.copies.create(questions: questions_gr.first(ques_nr), samples_nr: 1, student_id: sid, student_group_id: sgid)
             end
           end
         end
       end
     end
-
   end
 end
