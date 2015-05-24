2582741600e49e957c9acf8321a3888186663f23
Murzic <e.ungureanu92@gmail.com>
2015-05-24 20:50:27 +0300

    Changed the way quizzes are marked

--- app/jobs/scans_processing_job.rb
+++ app/jobs/scans_processing_job.rb
@@ -82,26 +82,33 @@ class ScansProcessingJob < ActiveJob::Base
   ########################################################
   def quiz_marking
     puts "Starting quiz marking.."
-    correct_answers = Array.new
     copy_answers = @copy.answers
-    questions = Question.where(id: @copy.questions).includes(:answers).where(answers: { correct: true })
+    questions = Question.where(id: @copy.questions).includes(:answers)
+    points = 0
+    total = 0
     questions.each do |q|
-      correct_answers = correct_answers + q.answer_ids
+      correct_answers = Array.new
+      incorrect_answers = Array.new
+      q.answers.each do |a|
+        if a.correct
+          correct_answers << a.id
+        else
+          incorrect_answers << a.id
+        end
+      end
+      correctly_ans = correct_answers & copy_answers
+      incorrectly_ans = incorrect_answers & copy_answers
+      answered = correctly_ans | incorrectly_ans
+
+      total += correct_answers.length
+      unless answered.length > correct_answers.length
+        points += correctly_ans.length
+      end
     end
 
-    ## Gets the correct chosen answers' ids
-    correct_answered = correct_answers & copy_answers
-    incorrect_answered = copy_answers - correct_answers
+    puts "Points/Total: #{points}/#{total}"
 
-    puts "Answered: #{copy_answers.length}"
-    p copy_answers
-    puts "Correct answers: #{correct_answers.length}"
-    p correct_answers
-    puts "Correct answered: #{correct_answered.length}"
-    p correct_answered
-    puts "Incorrect answered: #{incorrect_answered.length}"
-    p incorrect_answered
-    mark = (correct_answered.length-incorrect_answered.length).fdiv(correct_answers.length)*100
+    mark = points.fdiv(total)*100
 
     ## Updating mark field in copies table
     @copy.mark = mark
